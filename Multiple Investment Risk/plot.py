6c73770b702de871d5ff8fd466b56b84bc65e4ae
Ross <rtagaras@gmail.com>
2021-07-16 04:42:05 -0400

    updated plot

--- Multiple Investment Risk/plot.py
+++ Multiple Investment Risk/plot.py
@@ -7,9 +7,14 @@ ax.set_title("Stock Price Correlated to Market Scenario")
 ax.set(xlabel = "Days", ylabel = "Value")
 
 
-market = np.loadtxt("./Data/market.txt")
-stock = np.loadtxt("./Data/data.txt")
+market = np.loadtxt("./Data/market_scenario.txt")
+stock1 = np.loadtxt("./Data/security_1.txt")
+stock2 = np.loadtxt("./Data/security_2.txt")
 
-ax.plot(market)
-ax.plot(stock)
+
+ax.plot(market, label= "Market Scenario")
+ax.plot(stock1, label= "Security 1")
+ax.plot(stock2, label= "Security 2")
+ax.legend()
+
 plt.show()
