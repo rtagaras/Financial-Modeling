1535208fc0070808418dbeece4e8272c6c7e729b
Ross <rtagaras@gmail.com>
2021-09-03 03:46:35 -0400

    changed method of state generation

--- Options Pricing/american_boundary.cpp
+++ Options Pricing/american_boundary.cpp
@@ -34,26 +34,27 @@ double boundary(double t, double a1, double a2, double b1, double b2, double c1,
     return a1*std::log(b1*std::pow(t,c1) + 1.) + a2*std::log(b2*std::pow(t,c2) + 1.);
 }
 
-std::vector<double> generate_vector(double epsilon){
+std::vector<double> generate_vector(std::vector<double> x, double epsilon){
     /*
-    Gives the cartesian components of a random 6-dimensional vector with magnitude less than epsilon
+    Creates a random state "close" to x. This is done by sampling the 6-ball with radius epsilon centered at x.
     */
 
-    double r = gen_uniform(0, epsilon);
+    double r = gen_uniform(0, epsilon) + x[0];
     double theta_1 = gen_uniform(0, M_PI);
     double theta_2 = gen_uniform(0, M_PI);
     double theta_3 = gen_uniform(0, M_PI);
     double theta_4 = gen_uniform(0, M_PI);
     double theta_5 = gen_uniform(0, 2.*M_PI);
 
-    double x1 = r*cos(theta_1);
-    double x2 = r*sin(theta_1)*cos(theta_2);
-    double x3 = r*sin(theta_1)*sin(theta_2)*cos(theta_3);
-    double x4 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*cos(theta_4);
-    double x5 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*cos(theta_5);
-    double x6 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*sin(theta_5);
+    // double x1 = r*cos(theta_1);
+    // double x2 = r*sin(theta_1)*cos(theta_2);
+    // double x3 = r*sin(theta_1)*sin(theta_2)*cos(theta_3);
+    // double x4 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*cos(theta_4);
+    // double x5 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*cos(theta_5);
+    // double x6 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*sin(theta_5);
 
-    std::vector<double> v = {x1,x2,x3,x4,x5,x6};
+    // std::vector<double> v = {x1,x2,x3,x4,x5,x6};
+    std::vector<double> v = {r, theta_1, theta_2, theta_3, theta_4, theta_5};
 
     return v;
 }
@@ -97,19 +98,21 @@ class American_Option{
     }
 
     // Finds the optimal values of {a1, a2, b1, b2, c1, c2} so that the option price is maximized
-    std::pair<std::vector<double>, double> optimize_exercise_boundary(double a, double b, std::vector<double> x, double epsilon, int m, double T_max){
+    std::pair<std::vector<double>, double> optimize_exercise_boundary(double a, double b, double epsilon, int m, double T_max){
         
+        std::vector<double> x = generate_vector({0,0,0,0,0,0}, epsilon);
         std::vector<double> y;
-        double E = 1./GRW_price(x, 1);
+        double E = 1./GRW_price(x,1);
         double E_trial = 0;
         double delta_E = 0;
         double z = 0;
         double T = T_max;
 
         for(int i=0; i<m; i++){
+
             // create a random 6d vector with length less than epsilon. The vector components give the parameters for the current trial.
-            y = generate_vector(epsilon);
-            E_trial = 1./GRW_price(y,1);
+            y = generate_vector(x, epsilon);
+            E_trial = 1./GRW_price(y,100);
             delta_E = E_trial - E;
 
             // with probability e^{-Delta_E/T}, update the choice of vector
@@ -122,7 +125,7 @@ class American_Option{
             T = a*std::pow(b,i);
         }
 
-        return std::make_pair(x,1./GRW_price(x,1));
+        return std::make_pair(x, 1./GRW_price(x,100));
     }
 
     double GRW_price(std::vector<double> parameters, int m){
@@ -151,19 +154,18 @@ class American_Option{
 int main(){
     std::string type = "put";
     double strike = 100;
-    double expiry_time = 60/365.;
+    double expiry_time = 90/365.;
     double dt = 1/365.;
     double s_0 = 100;
     double r = 0.03;
     double mu = 0.03;
     double sigma = 0.2;
-    double epsilon = 0.1;
-    double a = 0.1;
+    double epsilon = 0.01;
+    double a = 0.5;
     double b = 0.999999999;
-    std::vector<double> x = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
     double T_max = 50;
 
     American_Option O = American_Option(type, strike, expiry_time, dt, s_0, r, mu, sigma);
-    std::pair<std::vector<double>, double> data = O.optimize_exercise_boundary(a, b, x, epsilon, 1000, T_max);
-
+    std::pair<std::vector<double>, double> data = O.optimize_exercise_boundary(a, b, epsilon, 10000, T_max);
+    std::cout << data.second << std::endl;
 }
