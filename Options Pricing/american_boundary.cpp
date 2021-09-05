1f3744f31cf676161fcc4319a6e098f6f1f2aba9
Ross <rtagaras@gmail.com>
2021-09-04 21:32:29 -0400

    changed method of state generation and function return error

--- Options Pricing/american_boundary.cpp
+++ Options Pricing/american_boundary.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <Eigen/Dense>
 #include <math.h>
+#include <cfloat>
 
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -37,25 +38,26 @@ double boundary(double t, double a1, double a2, double b1, double b2, double c1,
 std::vector<double> generate_vector(std::vector<double> x, double epsilon){
     /*
     Creates a random state "close" to x. This is done by sampling the 6-ball with radius epsilon centered at x.
+    The boundary parameters are supposed to be positive, so I took the absolute value. I'm not sure if this is better or worse than returning zero in the
+    case of a negative value. 
     */
 
-    double r = gen_uniform(0, epsilon) + x[0];
+    double r = gen_uniform(0, epsilon) + sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3] + x[4]*x[4] + x[5]*x[5]);
     double theta_1 = gen_uniform(0, M_PI);
     double theta_2 = gen_uniform(0, M_PI);
     double theta_3 = gen_uniform(0, M_PI);
     double theta_4 = gen_uniform(0, M_PI);
     double theta_5 = gen_uniform(0, 2.*M_PI);
 
-    // double x1 = r*cos(theta_1);
-    // double x2 = r*sin(theta_1)*cos(theta_2);
-    // double x3 = r*sin(theta_1)*sin(theta_2)*cos(theta_3);
-    // double x4 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*cos(theta_4);
-    // double x5 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*cos(theta_5);
-    // double x6 = r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*sin(theta_5);
+    // convert from spherical coordinates to cartesian
+    double x1 = abs(r*cos(theta_1));
+    double x2 = abs(r*sin(theta_1)*cos(theta_2));
+    double x3 = abs(r*sin(theta_1)*sin(theta_2)*cos(theta_3));
+    double x4 = abs(r*sin(theta_1)*sin(theta_2)*sin(theta_3)*cos(theta_4));
+    double x5 = abs(r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*cos(theta_5));
+    double x6 = abs(r*sin(theta_1)*sin(theta_2)*sin(theta_3)*sin(theta_4)*sin(theta_5));
 
-    // std::vector<double> v = {x1,x2,x3,x4,x5,x6};
-    std::vector<double> v = {r, theta_1, theta_2, theta_3, theta_4, theta_5};
-
+    std::vector<double> v = {x1,x2,x3,x4,x5,x6};
     return v;
 }
 
@@ -98,15 +100,17 @@ class American_Option{
     }
 
     // Finds the optimal values of {a1, a2, b1, b2, c1, c2} so that the option price is maximized
-    std::pair<std::vector<double>, double> optimize_exercise_boundary(double a, double b, double epsilon, int m, double T_max){
+    std::pair<std::vector<double>, double> optimize_exercise_boundary(double epsilon, double T_max, double dt){
         
-        std::vector<double> x = generate_vector({0,0,0,0,0,0}, epsilon);
+        std::vector<double> x = {gen_uniform(0,10),gen_uniform(0,10),gen_uniform(0,10),gen_uniform(0,10),gen_uniform(0,10),gen_uniform(0,10)};
+        
         std::vector<double> y;
-        double E = 1./GRW_price(x,1);
+        double E = 1./GRW_price(x,100);
         double E_trial = 0;
         double delta_E = 0;
         double z = 0;
         double T = T_max;
+        int m = T_max/dt;
 
         for(int i=0; i<m; i++){
 
@@ -122,18 +126,19 @@ class American_Option{
                 E = E_trial;
             }
 
-            T = a*std::pow(b,i);
+            T -= dt;
         }
 
-        return std::make_pair(x, 1./GRW_price(x,100));
+        return std::make_pair(x, 1/E);
     }
 
     double GRW_price(std::vector<double> parameters, int m){
         double sum = 0;
         double a1 = parameters[0], a2 = parameters[1], b1 = parameters[2], b2 = parameters[3], c1 = parameters[4], c2 = parameters[5];
-
+        double z;
+        
         for(int i=0; i<m; i++){
-            double z;
+            
             double s = s_0;
             for(int i=0; i<n; i++){
                 z = gen_norm(0,1);
@@ -144,28 +149,27 @@ class American_Option{
                     sum += exp(-r*n*dt)*payout(s);
                 }
             }
-            sum += exp(-r*expiry_time)*payout(s);
+            sum += payout(s);
         }
-        return sum/m;
+        return exp(-r*expiry_time)*sum/m;
     }
     
 };
 
 int main(){
+
     std::string type = "put";
     double strike = 100;
     double expiry_time = 90/365.;
     double dt = 1/365.;
     double s_0 = 100;
-    double r = 0.03;
-    double mu = 0.03;
-    double sigma = 0.2;
-    double epsilon = 0.01;
-    double a = 0.5;
-    double b = 0.999999999;
+    double r = 0.1;
+    double mu = 0.1;
+    double sigma = 0.3;
+    double epsilon = 0.1;
     double T_max = 50;
 
     American_Option O = American_Option(type, strike, expiry_time, dt, s_0, r, mu, sigma);
-    std::pair<std::vector<double>, double> data = O.optimize_exercise_boundary(a, b, epsilon, 10000, T_max);
+    std::pair<std::vector<double>, double> data = O.optimize_exercise_boundary(epsilon, T_max, 0.01);
     std::cout << data.second << std::endl;
 }
